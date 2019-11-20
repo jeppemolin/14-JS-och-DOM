62693e42f71ae27d6037505e51d61c008691199d
jeppemolin <57534314+jeppemolin@users.noreply.github.com>
2019-11-20 14:45:26 +0100

    Add files via upload

---
+++ uppgift 14 Javascript och Dom/testbed.js
@@ -0,0 +1,16 @@
+//javascript
+function changeText(){
+	var rubrik = document.getElementById('rubrik');
+	rubrik.innerHTML = 'Du clickade';
+}
+
+function changecolor(){
+	var contain = document.getElementById('contain').style.backgroundColor = "lightblue";
+	
+}
+function changecoloragain(){
+	var contain = document.getElementById('contain').style.backgroundColor = "lightgreen"
+}
+function changecoloragein(){
+	var contain = document.getElementById('contain').style.backgroundColor = "purple"
+}
